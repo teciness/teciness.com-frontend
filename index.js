6625070e418e2687c37a4351e1daeb40cbfd3cc8
CreeperExplosion <ghibranresearch@gmail.com>
2020-09-27 21:28:52 +0200

    fixed font weight and added scroll indicator

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 
 
 let navbar = document.getElementById("navbar")
+let heroIndicator = document.getElementById("hero-indicator")
 
 var navOff = navbar.offsetTop;
+var indOff = document.getElementById("indicator").offsetTop - 100;
 
-
+console.log(heroIndicator);
 window.onscroll = function stick() {
     
     if(window.pageYOffset >= navOff){
         navbar.classList.add("sticky");
     }
-    else {
-        navbar.classList.remove("sticky");
+
+    if( window.pageYOffset <= indOff){
+        heroIndicator.classList.add("bounce");
+        console.log("hello")
+    }else {
+        heroIndicator.classList.remove("bounce");
     }
+
+    
 
 }
