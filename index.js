7cd8f9d6759bf2be19f4c9deed00c180949a4e0f
CreeperExplosion <ghibranresearch@gmail.com>
2020-09-29 20:40:31 +0200

    added slideout (dropdown) menu

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let navbar = document.getElementById("navbar")
 let heroIndicator = document.getElementById("hero-indicator")
 
@@ -8,20 +6,65 @@ var indOff = document.getElementById("indicator").offsetTop - 100;
 
 console.log(heroIndicator);
 window.onscroll = function stick() {
-    
-    if(window.pageYOffset >= navOff){
+
+    if (window.pageYOffset >= navOff) {
         navbar.classList.add("sticky");
-    }else {
+    } else {
         navbar.classList.remove("sticky");
     }
 
-    if( window.pageYOffset <= indOff){
+    if (window.pageYOffset <= indOff) {
         heroIndicator.classList.add("bounce");
         console.log("hello")
-    }else {
+    } else {
         heroIndicator.classList.remove("bounce");
     }
 
-    
+
 
 }
+
+
+
+
+
+
+// This is the navbar interactivity
+
+
+let hamburgerClicked = false;
+
+let hamburger = document.getElementById("hamburger")
+let exit = document.getElementById("exit");
+let dropdown = document.getElementById("dropdown")
+
+
+
+hamburger.addEventListener("click", (e) => {
+
+    exit.classList.add("exit");
+
+    hamburger.classList.remove("hamburger");
+    hamburger.classList.add("invis");
+
+    dropdown.classList.remove("slide-out");
+
+    dropdown.classList.add("slide-in");
+
+})
+
+exit.addEventListener("click", (e) => {
+
+    hamburger.classList.add("hamburger");
+
+    exit.classList.add("invis");
+    exit.classList.remove("exit");
+
+
+    dropdown.classList.remove("slide-in");
+
+    dropdown.classList.add("slide-out");
+
+    setTimeout(() => dropdown.classList.remove("slide-out"), 300);
+
+})
